aa82a2cfc8766787ebed09f398be86e4e4c3d358
SarangChoudalwar <sarang.choudalwar@gmail.com>
2023-08-05 00:03:10 +0530

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   p7.c

--- pointer_and_array/p7.c
+++ pointer_and_array/p7.c
@@ -4,69 +4,71 @@ void disp(int a[][3][2], int jj, int kk, int ll);
 void show(int (*a)[3][2], int jj, int kk ,int ll);
 int main()
 {
-	int a[2][3][2]={
-					{
-						{1,2},
-						{3,4},
-						{5,6},	
-					},
-					{
-						{7,8},
-						{9,10},
-						{11,12},
-					},
-				};
+    int a[2][3][2]={
+                    {
+                        {1,2},
+                        {3,4},
+                        {5,6},	
+                    },
+                    {
+                        {7,8},
+                        {9,10},
+                        {11,12},
+                    },
+                    };
 
-	print(a,2,3,2);
-	disp(a,2,3,2);
-	show(a,2,3,2);
-	return 0;
+    print(a,2,3,2);
+    disp(a,2,3,2);
+    show(a,2,3,2);
+    return 0;
 }
 
 void print(int *a,int jj, int kk, int ll)
 {
-	for(int i=0;i<jj;i++)
-	{
-		for(int j=0;j<kk;j++)
-		{
-			for(int k=0;k<ll;k++)
-			{
-				printf("%d\t", *(a+i*kk*ll+j*ll+k));
-			}
-			printf("\n");
-		}
-		printf("\n");
-	}
+    for(int i=0;i<jj;i++)
+    {
+        for(int j=0;j<kk;j++)
+        {
+            for(int k=0;k<ll;k++)
+            {
+                printf("%d\t", *(a+i*kk*ll+j*ll+k));
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
 }
+
 void disp(int a[][3][2],int jj ,int kk, int ll)
 {
-	for(int i=0;i<jj;i++)
-	{
-		for(int j=0;j<kk;j++)
-		{
-			for(int k=0;k<ll;k++)
-			{
-				printf("%d\t", a[i][j][k]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-	}
+    for(int i=0;i<jj;i++)
+    {
+        for(int j=0;j<kk;j++)
+        {
+            for(int k=0;k<ll;k++)
+            {
+                printf("%d\t", a[i][j][k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
 }
+
 void show(int (*a)[3][2],int jj,int kk, int ll)
 {
-	for(int i=0;i<jj;i++)
-	{
-		for(int j=0;j<kk;j++)
-		{
-			int *temp = a[i][j];
+    for(int i=0;i<jj;i++)
+    {
+        for(int j=0;j<kk;j++)
+        {
+            int *temp = a[i][j];
 			
-			for(int k=0;k<ll;k++)
-			{
-				printf("%d\t",*(temp+k));
-			}
-			printf("\n");
-		}
-		printf("\n");
-	}
+            for(int k=0;k<ll;k++)
+            {
+                printf("%d\t",*(temp+k));
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
 }
