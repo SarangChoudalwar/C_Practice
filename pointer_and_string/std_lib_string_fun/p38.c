edca4cbcc31f103e53b62ae5d08e64d67398fcc8
SarangChoudalwar <sarang.choudalwar@gmail.com>
2023-08-12 23:42:15 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   p38.c

--- pointer_and_string/std_lib_string_fun/p38.c
+++ pointer_and_string/std_lib_string_fun/p38.c
@@ -1,16 +1,30 @@
 #include<stdio.h>
 int xstrlen(char *str);
+void xstrcpy(char *t, char *s);
+
 int main()
 {
     char arr[]="Bamboozled";
+    char arr2[25];
     int len1,len2;
 
     len1= xstrlen(arr);
     len2= xstrlen("HumptyDumpty");
+    xstrcpy(arr2,arr);    
     printf("string is %s, len is %d\n", arr, len1);
     printf("string is %s, len is %d\n","HumptyDumpty", len2);
-    
+    printf("string after copying is %s\n", arr2); 
     return 0;
+}
+
+void xstrcpy(char *target, char *source)
+{
+    while(*source != '\0')
+    {
+        *target = *source;
+        target++;
+        source++;
+    }
 }
 
 int xstrlen(char *str)
