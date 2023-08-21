fa2fbc8d46f06c7d4edd60bd0711a7a8454a20b3
SarangChoudalwar <sarang.choudalwar@gmail.com>
2023-08-21 10:19:07 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   A_2.c

---
+++ pointer_and_string/A_2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf(5+"Fascimile\n");
+    return 0;
+}
+
+/*printf takes the base address adding 5 to the base address points the pointer to 5th offset and hence sould start printing from letter "m" */
